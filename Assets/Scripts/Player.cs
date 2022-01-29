fc8c5a6e7f09c152e5d28a90e2ef68d268793e53
SolAZDev <SolAZDev@gmail.com>
2022-01-29 04:34:17 -0400

    Core player mecanics

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,7 +11,8 @@ public class Player : Creature
 
     Rigidbody rigid;
     public Animator LightChar, DarkChar;
-    Animator animator;
+    public Rigidbody LightBody;
+    // Animator animator;
     Vector2 jDir = Vector2.zero;
     // Start is called before the first frame update
     void Start()
@@ -22,31 +23,49 @@ public class Player : Creature
     // Update is called once per frame
     void Update()
     {
-        animator.SetFloat("MoveRun", jDir.magnitude);
+        // animator.SetFloat("MoveRun", jDir.magnitude);
         if (jDir.magnitude > .1f)
         {
             Vector3 CamForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
-            rigid.AddForce((jDir.y * CamForward + jDir.x * Camera.main.transform.right) * (8 * jDir.magnitude), ForceMode.Acceleration);
+            rigid.AddForce((jDir.y * CamForward + jDir.x * Camera.main.transform.right) * (8 * jDir.magnitude), ForceMode.Force);
         }
     }
 
+    public void ThrowLight()
+    {
+        LightBody.transform.parent = null;
+        LightBody.AddForce((transform.forward * hj 1) + (transform.up * 5), ForceMode.Impulse);
+    }
+
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.transform.tag == "PlayerLightEmitter")
+        {
+            if (LightBody != null) return;
+            LightBody = other.gameObject.GetComponent<Rigidbody>();
+            LightBody.transform.parent = this.transform;
+            LightBody.transform.position = ((transform.position) + (transform.forward * 2));
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "PlayerLight") SwitchLayers();
+        if (other.tag == "PlayerLight") SwitchLayers(false);
         if (other.tag == "LightArea" && isDark) Hurt();
     }
     private void OnTriggerExit(Collider other)
     {
-        if (other.tag == "PlayerLight") SwitchLayers();
+        if (other.tag == "PlayerLight") SwitchLayers(true);
     }
 
 
-    public void SwitchLayers()
+    public void SwitchLayers(bool enableDark)
     {
-        isDark = !isDark;
+        print("Switching");
+        isDark = enableDark;
         LightChar.gameObject.SetActive(!isDark);
         DarkChar.gameObject.SetActive(isDark);
-        animator = isDark ? DarkChar : LightChar;
+        // animator = isDark ? DarkChar : LightChar;
         //Play Effect
         LevelManager.instance.ToggleWorld(isDark);
     }
@@ -54,6 +73,7 @@ public class Player : Creature
 
     #region Input System
     public void OnMove(InputValue dir) => jDir = dir.Get<Vector2>();
-    public void OnJump() => rigid.AddForce(Vector3.up, ForceMode.Impulse);
+    public void OnJump() => rigid.AddForce(Vector3.up * 8, ForceMode.Impulse);
+    public void OnThrow() => ThrowLight();
     #endregion
 }
